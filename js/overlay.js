cb49abeea8c7564908637f20dfda2c3ade45f2dc
B Poh <benedictcheryl@users.noreply.github.com>
2019-08-13 22:33:07 +0800

    added overlay for invite code

---
+++ js/overlay.js
@@ -0,0 +1,16 @@
+function loadpage() {
+    var boxoverlay = document.getElementById("overlay");
+    var testV = 1;
+    var pass1 = prompt('Please Enter Your Invite Code', 'Invite Code');;
+    while (testV < 3) {
+        if (pass1.toLowerCase() == "wedding") {
+            boxoverlay.classList.add("disappear")
+            window.scrollTo(0, 0);
+            break;
+        }
+        testV += 1;
+        pass1 = prompt('Input Code Not Recognized, Please Try Again', 'Invite Code');
+    }
+    history.go(-1);
+    return " ";
+}
