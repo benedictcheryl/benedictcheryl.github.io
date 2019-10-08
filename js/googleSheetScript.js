4c89d439426f25b9073d07fa6a0ce0fc2eaadb45
B Poh <benedictcheryl@users.noreply.github.com>
2019-10-08 16:12:41 +0800

    Update googleSheetScript.js

--- js/googleSheetScript.js
+++ js/googleSheetScript.js
@@ -1,5 +1,5 @@
 var $form = $('form#test-form'),
-url = 'https://script.google.com/macros/s/abcdefghijklmnopqrstuvwxyz1234567890/exec'
+url = 'https://script.google.com/macros/s/AKfycbzgJWtB6i8UKXMniWI7ITCJztTyd7HfaegflIy5PYE2u0OunxEw/exec'
 
 $('#submit-form').on('click', function(e) {
 e.preventDefault();
