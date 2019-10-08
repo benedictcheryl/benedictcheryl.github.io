ab2df4584ce83503e8b1adf5fed940b29f0b3ba5
B Poh <benedictcheryl@users.noreply.github.com>
2019-10-08 17:01:42 +0800

    k

--- js/googleSheetScript.js
+++ js/googleSheetScript.js
@@ -1,11 +1,12 @@
-var $form = $('form#test-form'),
+var $form = $('form#rsvpForm'),
 url = 'https://script.google.com/macros/s/AKfycbzgJWtB6i8UKXMniWI7ITCJztTyd7HfaegflIy5PYE2u0OunxEw/exec'
 
 $('#submit-form').on('click', function(e) {
+alert("Success!");
 e.preventDefault();
 var jqxhr = $.ajax({
 url: url,
-method: "GET",
+method: "POST",
 dataType: "json",
 data: $form.serializeObject()
 }).success(
